0ff85b7592de2431fd737f9fe864657d956c0755
Den1sprokrastinator <puteec228@gmail.com>
2022-07-05 21:28:10 +0300

    api  v.2

---
+++ src/test/java/core/Endpoints.java
@@ -0,0 +1,16 @@
+package core;
+
+public class Endpoints {
+
+    //project endpoints
+
+    public static final String GET_RUN = "index.php?/api/v2/get_run/{run_id}";
+    public static final String GET_RUNS = "index.php?/api/v2/get_runs/{project_id}";
+    public static final String ADD_RUN = "index.php?/api/v2/add_run/{project_id}";
+
+    //milestone endpoints
+
+
+
+
+}
