396aeaa74878e39f1e3a2b4fa6166203994d9e65
Den1sprokrastinator <puteec228@gmail.com>
2022-07-08 16:30:50 +0300

    ver2

--- src/test/java/helpers/TestRunsAndResultsHelper.java
+++ src/test/java/helpers/TestRunsAndResultsHelper.java
@@ -4,6 +4,8 @@ import core.Endpoints;
 import models.TestRuns;
 import org.apache.http.HttpStatus;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class TestRunsAndResultsHelper {
@@ -11,7 +13,7 @@ public class TestRunsAndResultsHelper {
 
     public TestRuns getProject(int testRun) {
         return given()
-                .pathParam("run_id",1)
+                .pathParam("run_id",testRun)
                 .get(Endpoints.GET_RUN)
                 .then()
                 .assertThat()
@@ -20,6 +22,33 @@ public class TestRunsAndResultsHelper {
                 .extract()
                 .as(TestRuns.class);
     }
+
+    public TestRuns getProjects(int projectID){
+        return  given()
+                .pathParams("project_id",2)
+                .get(Endpoints.GET_RUNS)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .log().body()
+                .extract()
+                .as(TestRuns.class);
+    }
+
+    public TestRuns addProject(int projectID,Map jsonMap){
+
+       return   given()
+                .pathParams("project_id", projectID)
+                .body(jsonMap)
+                .post(Endpoints.ADD_RUN)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .log().body()
+                .extract()
+                .as(TestRuns.class);
+    }
+
 
 
 
--- src/test/java/tests/api/ApiTest.java
+++ src/test/java/tests/api/ApiTest.java
@@ -1,12 +1,35 @@
 package tests.api;
 
 import baseEntities.BaseApiTest;
+import models.TestRuns;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ApiTest extends BaseApiTest {
     @Test
-    public void test(){
+    public void get() {
 
-testRunsAndResultsHelper.getProject(1);
+        testRunsAndResultsHelper.getProject(2);
+
+
+    }
+
+    @Test
+    public void gets() {
+        testRunsAndResultsHelper.getProjects(2);
+    }
+
+    @Test
+    public void add() {
+        TestRuns milestone = TestRuns.builder()
+                .name("DenisMilestoneAPI")
+                .build();
+        Map<String, Object> jsonMap = new HashMap<>();
+
+        jsonMap.put("name", milestone.getName());
+
+        testRunsAndResultsHelper.addProject(2, jsonMap);
     }
 }
