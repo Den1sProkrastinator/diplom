3055500aa5bf3d12dfebe8a8854094ea641165a5
Den1sprokrastinator <puteec228@gmail.com>
2022-07-11 14:33:55 +0300

    ver2

--- src/test/java/tests/api/ApiTest.java
+++ src/test/java/tests/api/ApiTest.java
@@ -1,13 +1,21 @@
 package tests.api;
 
+
 import baseEntities.BaseApiTest;
+import core.ReadProperties;
+import helpers.ProjectHelper;
+import helpers.TestRunsAndResultsHelper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import models.TestRuns;
+import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.lang.Integer.parseInt;
+import static io.restassured.RestAssured.given;
 
 public class ApiTest extends BaseApiTest {
     public int projectId;
@@ -48,6 +56,33 @@ public class ApiTest extends BaseApiTest {
         Map<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("name", milestone.getName());
         projectId= projectHelper.addProject(jsonMap);
+
+    }
+
+    @Test
+    public void addRun1() {
+        RestAssured.baseURI = ReadProperties.getUrl();
+        RestAssured.requestSpecification = given()
+                .header(HTTP.CONTENT_TYPE, ContentType.JSON)
+                .auth().preemptive().basic(ReadProperties.username(), ReadProperties.password());
+
+        testRunsAndResultsHelper = new TestRunsAndResultsHelper();
+        projectHelper = new ProjectHelper();
+        TestRuns testRun = TestRuns.builder()
+                .name("TestRun")
+                .build();
+        Map<String, Object> jsonMap = new HashMap<>();
+
+        jsonMap.put("name", testRun.getName());
+
+        given()
+
+                .body(jsonMap)
+                .post("index.php?/api/v2/add_run/20")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .log().body();
 
     }
 }
